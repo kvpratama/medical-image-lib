c4f5dd75518e0b4a8e504c34fa570ceacd12aba0
Kevin Pratama <kvpratama@gmail.com>
2020-10-07 18:12:27 +0900

    simplify Nii and Nii3D class

--- nii/Nii.py
+++ nii/Nii.py
@@ -1,5 +1,4 @@
 import nibabel as nib
-import numpy as np
 
 
 class Nii:
@@ -7,11 +6,9 @@ class Nii:
     def __init__(self, path):
         self.path = path
         self.nii = nib.load(self.path)
-        self.nii_np = np.array(self.nii.dataobj)
 
     def plot(self, index=0):
         pass
 
     def save(self, save_path):
-        nii = nib.Nifti1Image(self.nii_np, affine=None)
-        nib.save(nii, save_path)
+        nib.save(self.nii, save_path)
--- nii/Nii3D.py
+++ nii/Nii3D.py
@@ -6,22 +6,9 @@ import matplotlib.pyplot as plt
 
 
 class Nii3D(Nii):
-    def __init__(self, path, orientation='axial'):
+    def __init__(self, path):
         Nii.__init__(self, path)
-
-        self.orientation = orientation
-
-        if self.orientation == 'axial':
-            self.nii_np = np.transpose(self.nii_np, axes=[2, 1, 0])
-        elif self.orientation == 'coronal':
-            self.nii_np = np.transpose(self.nii_np, axes=[1, 2, 0])
-        elif self.orientation == 'sagittal':
-            self.nii_np = np.transpose(self.nii_np, axes=[0, 2, 1])
-        else:
-            print('Orientation {} is not supported. Using axial orientation.'.format(orientation))
-            self.nii_np = np.transpose(self.nii_np, axes=[2, 1, 0])
-            self.orientation == 'axial'
-
+        self.nii_np = np.transpose(np.array(self.nii.dataobj), axes=[2, 1, 0])
         self.shape = self.nii_np.shape
 
     def plot(self, index=0):
@@ -29,12 +16,6 @@ class Nii3D(Nii):
         plt.show()
 
     def save(self, save_path):
-        if self.orientation == 'axial':
-            nii = nib.Nifti1Image(np.transpose(self.nii_np, axes=[2, 1, 0]), affine=None)
-        elif self.orientation == 'coronal':
-            nii = nib.Nifti1Image(np.transpose(self.nii_np, axes=[2, 1, 0]), affine=None)
-        elif self.orientation == 'sagittal':
-            nii = nib.Nifti1Image(np.transpose(self.nii_np, axes=[2, 1, 0]), affine=None)
-
+        nii = nib.Nifti1Image(np.transpose(self.nii_np, axes=[2, 1, 0]), affine=None)
         nib.save(nii, save_path)
 
